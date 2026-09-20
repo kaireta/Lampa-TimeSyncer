084bb808c7e5efd3ed75cc92501b10cede98f91a
kaireta <kaireta@github.com>
2026-09-21 02:06:49 +0300

    feat: rename tab to Сервисы and add Netflix

--- rus_movie_clean.js
+++ rus_movie_clean.js
@@ -1,4 +1,4 @@
-﻿(function () {
+(function () {
     'use strict';
 
     var plugin_name = 'rus_movie_clean';
@@ -12,8 +12,10 @@
         { title: 'Русские сериалы', img: 'https://bylampa.github.io/img/rus_tv.jpg', request: 'discover/tv?sort_by=first_air_date.desc&with_original_language=ru&air_date.lte=' + today },
         { title: 'Русские мультфильмы', img: 'https://bylampa.github.io/img/rus_mult.jpg', request: 'discover/movie?vote_average.gte=5&vote_average.lte=9.5&with_genres=16&with_original_language=ru&sort_by=primary_release_date.desc&primary_release_date.lte=' + today },
         { title: 'Ток-шоу и Реалити', img: 'https://cdn.jsdelivr.net/gh/kaireta/Lampa-TimeSyncer@main/talk_shows.jpg', request: 'discover/tv?with_original_language=ru&with_genres=10767,10764&sort_by=popularity.desc' },
+        { title: 'Netflix', img: 'https://cdn.jsdelivr.net/gh/kaireta/Lampa-TimeSyncer@main/netflix.jpg', request: 'discover/tv?with_networks=213&sort_by=first_air_date.desc&air_date.lte=' + today },
         { title: 'Start', img: 'https://bylampa.github.io/img/start.jpg', request: 'discover/tv?with_networks=3923&sort_by=first_air_date.desc&air_date.lte=' + today },
         { title: 'Premier', img: 'https://bylampa.github.io/img/premier.jpg', request: 'discover/tv?with_networks=2859&sort_by=first_air_date.desc&air_date.lte=' + today },
+
         { title: 'KION', img: 'https://bylampa.github.io/img/kion.jpg', request: 'discover/tv?with_networks=4085&sort_by=first_air_date.desc&air_date.lte=' + today },
         { title: 'ИВИ', img: 'https://bylampa.github.io/img/ivi.jpg', request: 'discover/tv?with_networks=3871&sort_by=first_air_date.desc&air_date.lte=' + today },
         { title: 'Okko', img: 'https://bylampa.github.io/img/okko.jpg', request: 'discover/tv?with_networks=2493&sort_by=first_air_date.desc&air_date.lte=' + today },
@@ -72,10 +74,11 @@
 
     function addMenuEntry() {
         var icon = '<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-width="4"><path stroke-linejoin="round" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z"/><path stroke-linejoin="round" d="M24 18a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm0 18a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm-9-9a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm18 0a3 3 0 1 0 0-6a3 3 0 0 0 0 6Z"/><path stroke-linecap="round" d="M24 44h20"/></g></svg>';
-        var li = $('<li class="menu__item selector" data-action="rus_movie"><div class="menu__ico">' + icon + '</div><div class="menu__text">Русское</div></li>');
-        li.on('hover:enter click', function () { Lampa.Activity.push({ url: '', title: 'Русское', component: 'rus_movie_clean' }); });
+        var li = $('<li class="menu__item selector" data-action="rus_movie"><div class="menu__ico">' + icon + '</div><div class="menu__text">Сервисы</div></li>');
+        li.on('hover:enter click', function () { Lampa.Activity.push({ url: '', title: 'Сервисы', component: 'rus_movie_clean' }); });
         
         function inject() {
+
             var nav = document.querySelector('.menu ul, .menu__list, .navigation__items');
             if (nav) $(nav).append(li);
         }
