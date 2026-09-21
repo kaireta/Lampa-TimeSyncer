3164d59a677aaa9927a5787b2b371024fbb656bd
kaireta <kaireta@github.com>
2026-09-21 03:58:54 +0300

    fix(vk): change network.request to network.silent

--- vkv.js
+++ vkv.js
@@ -8,7 +8,7 @@
     function parseVK(query, onsuccess, onerror) {
         var network = new Lampa.Reguest();
         var url = 'https://api.allorigins.win/raw?url=' + encodeURIComponent('https://vk.com/al_video.php?act=search_video&al=1&q=' + encodeURIComponent(query));
-        network.request(url, function(res) {
+        network.silent(url, function(res) {
             try {
                 var jsonStr = res;
                 var start = jsonStr.indexOf('{"payload"');
@@ -65,7 +65,7 @@
     function extractHLS(iframeUrl, onsuccess, onerror) {
         var network = new Lampa.Reguest();
         var url = 'https://api.allorigins.win/raw?url=' + encodeURIComponent(iframeUrl);
-        network.request(url, function(res) {
+        network.silent(url, function(res) {
             var m = res.match(/"hls":"([^"]+)"/);
             if (m && m[1]) {
                 var hls = m[1].replace(/\\\//g, '/');
